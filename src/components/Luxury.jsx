1162a8cc22449f9ecc9ac5fc7ee78ebe7fff7e75
Igor Miguel <igormedeirosmiguel@gmail.com>
2024-03-17 00:29:11 -0300

    Correção de erro e projeto finalizado

--- src/components/Luxury.jsx
+++ src/components/Luxury.jsx
@@ -4,7 +4,7 @@ export default function Luxury() {
     <section className="bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg">
         <img src={iconLuxury} alt="icone carros de luxo" className="mb-7"/>
         <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxuosos</h2>
-        <p className="font-Lexend mb-7 text-offwhite lg:mb-20">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+        <p className="font-Lexend mb-7 text-offwhite lg:mb-20">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo</p>
         <a className="font-Lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-dark-green border-2 border-offwhitebg hover:bg-dark-green hover:text-offwhitebg" href="https://www.webmotors.com.br/carros-de-luxo" target="_blanck">Ver mais  </a>
     </section>
     )
